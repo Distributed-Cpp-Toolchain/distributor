bf4b1a1fa2d55d091d793305580d5f6d2e0ae27a
Nikita Paniukhin <git@npanuhin.me>
2025-02-25 02:20:07 +0100

    Getting GCC version

--- src/compilers/GCC.py
+++ src/compilers/GCC.py
@@ -1,10 +1,28 @@
+import sys
+import re
+
+if __name__ == '__main__':
+    sys.path.append('../')
+
 from src.Actions import Action, RunProcess
+from src.utils import unsupported
+
+
+GCC_VERSION_REGEX = r'g(?:cc|\+\+).+?(\d+\.\d+\.\d+)'
 
 
 class GCC(Action):
     def __init__(self, arguments: list[str]):
         self.executable = arguments[0]
         self.arguments = arguments[1:]
+
+        print(self.get_version())
+
+    def get_version(self) -> str:
+        output, error = RunProcess([self.executable, '--version']).execute()
+        match = re.fullmatch(GCC_VERSION_REGEX, output.splitlines()[0], re.IGNORECASE)
+        assert not error and match, unsupported(f'gcc --version:\n{output}\n{error}')
+        return match.group(1)
 
     def get_dependencies(self):
         pass
--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,7 @@ import os
 
 def mkpath(*paths: str) -> str:
     return os.path.normpath(os.path.join(*paths))
+
+
+def unsupported(message: str) -> str:
+    return f'Encountered unsupported feature, please report all the details to the developers:\n{message}'
